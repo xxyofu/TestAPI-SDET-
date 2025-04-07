23ff1d62ba72345fd42c2c56dd300a653b846979
Xxyofu <xxyofu@gmail.com>
2025-04-07 13:49:55 +0300

    Remove ListEntities

--- src/test/java/tests/GetAllTest.java
+++ src/test/java/tests/GetAllTest.java
@@ -3,12 +3,12 @@ package tests;
 import helpers.BaseRequests;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
-import io.restassured.mapper.ObjectMapperType;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pojo.GetEntity;
-import pojo.ListEntities;
 
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -20,23 +20,24 @@ public class GetAllTest extends BaseTest{
     public void GetAllEntity_test(){
         GetEntity createEntity = GetEntity.builder().title("Test Get").build();
         UserId = BaseRequests.CreateEntity(createEntity, requestSpecification);
-        ListEntities getEntities = given()
+        List<GetEntity> getEntities = given()
                 .spec(requestSpecification)
                 .when()
                 .get("/api/getAll")
                 .then()
                 .statusCode(200)
                 .extract()
-                .as(ListEntities.class, ObjectMapperType.GSON);
+                .jsonPath()
+                .getList("entity", GetEntity.class);
         SoftAssert softAssertation = new SoftAssert();
         boolean flag = false;
-        for (int i = 0; i<getEntities.getEntity().length; i++){
-            if (getEntities.getEntity()[i].getId()==UserId) {
-                softAssertation.assertEquals(getEntities.getEntity()[i].getId(), UserId);
+        for (int i = 0; i<getEntities.size(); i++){
+            if (getEntities.get(i).getId()==UserId) {
+                softAssertation.assertEquals(getEntities.get(i).getId(), UserId);
                 flag = true;
             }
         }
-        if (flag==false)
+        if (!flag)
             softAssertation.assertEquals(UserId, -1);
     }
 
